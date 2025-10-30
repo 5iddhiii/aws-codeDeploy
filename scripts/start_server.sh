112dc5f4510d9bde79da29b3b59383ba032f3df0
5iddhiii <itsssiddhi@gmail.com>
2025-10-31 01:57:10 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "Deployment successful! Apache started."
+echo "Deployment successful! Apache started." > /var/www/html/deploy_log.txt
